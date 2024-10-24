a2d19366e7a2c97248b8f2c27b0bdd504bcf0dd6
Irene San Roman <irenesrfuentes@gmail.com>
2024-10-24 17:48:37 +0200

    uncionamiento SearchBox

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -9,5 +9,12 @@ export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void{
+    this.onValue.emit( value );
+  }
 
 }
