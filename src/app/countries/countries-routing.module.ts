e80d77dc1dd78f1627565049b767bb904ec6b9fa
Irene San Roman <irenesrfuentes@gmail.com>
2024-10-24 16:15:55 +0200

    Countries Routing Module

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,38 @@
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+
+const routes: Routes = [
+  {
+    path: 'by-capital',
+    component: ByCapitalPageComponent
+  },
+  {
+    path: 'by-country',
+    component: ByCountryPageComponent
+  },
+  {
+    path: 'by-region',
+    component: ByRegionPageComponent
+  },
+  {
+    path: 'by/:id',
+    component: CountryPageComponent
+  },
+
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forChild (routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+
+})
+export class CountriesRoutingModule { }
