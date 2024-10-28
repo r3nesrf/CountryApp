3ad202654135555dd2d8ef12836009757734bf96
Irene San Roman <irenesrfuentes@gmail.com>
2024-10-28 11:31:55 +0100

    Fin Sección 9

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -6,6 +6,7 @@ import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -14,7 +15,8 @@ import { CountriesRoutingModule } from './countries-routing.module';
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
